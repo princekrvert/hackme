99be1b5346176f638677cc481ac3a7d7ccc24493
Princekrvert <princekrvert@gmail.com>
2023-06-21 17:06:55 +0530

    second update in ctf

--- ctf.sh
+++ ctf.sh
@@ -9,6 +9,12 @@ user_inttrupt(){
 echo -e "\033[31;1m Exiting ctf tool.."
 exit 1
 }
+# Make a function for the About me 
+about_me(){
+echo -e "\e[32;1m Hi there , i am Prince Kumar a junior mechanical enginner. i am intrested in cyber security."
+echo -e "\e[32;1m Here is my social media links if you want to contact me."
+echo -e "\e[30;1m Instagram : https://instagram.com/princekrvert \n Facebook : https://facebook.com/princekrvet \n Telegram : t.me/princekrvert"
+}
 # now make a banner for this tool 
 banner(){
 echo -e "This is demo banner "	
@@ -21,3 +27,20 @@ echo -e "\033[35;1m[2] \033[33;0m Category"
 echo -e "\033[35;1m[3] \033[33;0m Update "
 echo -e "\033[35;1m[4] \033[33;0m About me"
 echo -e "\033[35;1m[5] \033[33;0m Exit"
+# NOw read the answer form the user
+read user_f
+if [[ $user_f == "1" ]];then
+	echo -e "\033[30;1m All called"
+elif [[ $user_f == "2" ]];then 
+	echo -e "\033[30;1m Category called"
+elif [[ $user_f == "3" ]];then
+	echo -e "\033[30;1m Update "
+elif [[ $user_f == "4" ]];then
+	about_me
+elif [[ $user_f == "5" ]];then 
+	echo -e "\033[30;1m Exit"
+else 
+	echo -e "\033[31;1m Exiting.. "
+	exit 1 
+fi
+
